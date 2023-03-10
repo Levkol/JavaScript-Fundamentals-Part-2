f01167628b4f2825cdd1347c332f4d6dc9adf4e7
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-03-10 07:51:47 +0100

    Project files
    It contains the basics of functions and the introduction of arrays.

---
+++ assignment.js
@@ -0,0 +1,113 @@
+"use strict";
+
+/*--------------------------1 FUNCTIONS-------------------------*/
+/*
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} people and it's capital city is ${capitalCity}.`;
+}
+
+// function describeCountry(country, population, capitalCity) {
+//   const summery = `${country} has ${population} people and it's capital city is ${capitalCity}.`;
+//   return summery;
+// }
+// console.log(describeCountry(`Hungary`, 9.71, `Budapest`));
+
+const Hungary = describeCountry(`Hungary`, 9.71, `Budapest`);
+const Findland = describeCountry(`Findland`, 6, `Helsinki`);
+const Portugal = describeCountry(`Portugal`, 10.33, `Lisbon`);
+
+// console.log(Hungary, Findland, Portugal);
+console.log(Hungary);
+console.log(Findland);
+console.log(Portugal);
+*/
+
+/*--------------------------2 FUNCTION DECLARATIONS VS EXPRESSIONS-------------------------*/
+/*
+//Function Declaration:
+
+function percentageOfWorld1(country, population) {
+  return `${country} has ${population} million people, so it's about ${(population / 7900) * 100}% of the world population.`;
+}
+
+// const population1 = percentageOfWorld1(`China`, 1441);
+// const population2 = percentageOfWorld1(`Hungary`, 9.71);
+// const population3 = percentageOfWorld1(`Portugal`,10.33);
+// console.log(population1);
+// console.log(population2);
+// console.log(population3);
+
+
+//Function Expression
+const percentageOfWorld2 = function (country, population){
+  return `${country} has ${population} million people, so it's about ${(population / 7900) * 100}% of the world population.`;
+}
+
+const population4 = percentageOfWorld2(`China`, 1441);
+const population5 = percentageOfWorld2(`Hunagry`, 9.71);
+const population6 = percentageOfWorld2(`Portugal`, 10.33);
+console.log(population4);
+console.log(population5);
+console.log(population6);
+*/
+
+
+
+/*--------------------------3 ARROW FUNCTIONS-------------------------*/
+/*
+const percentageOfWorld3 = (country, population) => {
+  return `${country} has ${population} million people, so it's about ${(population/ 7900) * 100}% of the world population.`;
+}
+
+console.log(percentageOfWorld3(`Hungary`, 9.71));
+*/
+
+
+/*--------------------------4 FUNCTIONS CALLING OTHER  FUNCTIONS-------------------------*/
+/*
+const percentageOfWorld1 = function(population){
+  return (population / 7900) * 100;
+}
+
+// const describePopulation = function(country, population){
+//   const percentage = percentageOfWorld1(population);
+//   const description = `${country} has ${population} million people, which is about ${percentage}% of the world`;
+//   return description;
+// };
+
+// console.log(describePopulation(`China`, 1441));
+
+const describePopulation = function (country, population) {
+  const percentage = percentageOfWorld1(population);
+  const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+  console.log(description);
+  };
+
+describePopulation('Portugal', 10);
+describePopulation('China', 1441);
+describePopulation('USA', 332);
+*/
+
+
+/*--------------------------5 INTRODUCTION TO ARRAYS-------------------------*/
+/*
+const populations = [9.71, 10, 1441, 332];
+console.log(populations.length === 4);
+
+
+const percentageOfWorld1 = function(populations){
+  return (populations / 7900) * 100;
+}
+
+const percentages = [
+  percentageOfWorld1(populations[0]), 
+  percentageOfWorld1(populations[1]), 
+  percentageOfWorld1(populations[2]), 
+  percentageOfWorld1(populations[populations.length - 1])
+];
+console.log(percentages);
+*/
+
+
+
+/*--------------------------6 -------------------------*/
---
+++ script.js
@@ -0,0 +1,253 @@
+/*--------------------------1 STRICT MODE-------------------------*/
+"use strict";
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`I can drive :D`);
+
+const inferface = "Audio";
+const private = 435;
+*/
+
+/*--------------------------2 FUNCTIONS-------------------------*/
+/*
+function logger() {
+  console.log(`My name is Jonas.`);
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(3, 3);
+console.log(appleOrangeJuice);
+*/
+
+/*--------------------------3 FUNCTION DECLARATIONS VS EXPRESSIONS-------------------------*/
+/*
+// function calcAge1(birthYear) {
+//   const age = 2037 - birthYear;
+//   return age;
+// }
+
+//Function Declarations
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+//Function Expressions
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+*/
+
+/*--------------------------4 ARROW FUNCTIONS-------------------------*/
+/*
+// //Function Expression
+// const calcAge2 = function (birthYear){
+// return 2037 - birthYear;
+// }
+
+
+//Arrow Function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037- birthYear;
+  const retirement = 65 - age;
+  //return retirement;
+  return `${firstName} retires in ${retirement} years.`;
+}
+
+console.log(yearsUntilRetirement(1991, `Jonas`));
+console.log(yearsUntilRetirement(1998, `Levi`));
+*/
+
+
+
+/*--------------------------5 FUNCTIONS CALLING OTHER  FUNCTIONS-------------------------*/
+/*
+function cutFruitPieces(fruit){
+  return fruit * 4;
+}
+
+
+function fruitProcessor(apples, oranges){
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} piece of oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2,3));
+*/
+
+
+
+/*--------------------------6 REVIEWING FUNCTIONS-------------------------*/
+/*
+//CONVERT THIS ARROW FUNTION TO A NORMAL FUNCTION EXPRESSION:
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - birthYear;
+//   return `${firstName} retires in ${retirement} years.`;
+// }
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - birthYear;
+//   return `${firstName} retires in ${retirement} years.`;
+// }
+
+//CONVERT THIS FUNTION EXPRESSION TO DOUBLE FUNTCION
+
+const calcAge = function (year){
+  return 2037 -  year;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge (birthYear);
+  const retirement = 65 - age;
+
+  if(retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years.`);
+    return retirement;
+  }else{
+    console.log(`${firstName} has already retired! ✌️`);
+  return -1;
+  }
+}
+
+console.log(yearsUntilRetirement(1991, `Jonas`));
+console.log(yearsUntilRetirement(1950, `Mike`));
+*/
+
+
+
+/*--------------------------1 CODING CHALLENGE-------------------------*/
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above.
+
+Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and Data 2
+5. Ignore draws this time
+
+Test data:
+§ Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+§ Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+Hints:
+§ To calculate average of 3 values, add them all together and divide by 3
+§ To check if number A is at least double number B, check for A >= 2 * B.
+
+Apply this to the team's average scores 😉
+
+GOOD LUCK 😀
+
+
+
+const calcAverage = (firstScore, secondScore, thirdScore) => {
+ return (firstScore + secondScore + thirdScore) / 3;
+}
+
+//Data1:
+let averageDolphines = (calcAverage(44, 23, 71)); //Dolphines
+let averageKoalas = (calcAverage(65, 54, 49)); //Koalas
+console.log(averageDolphines, averageKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  
+  if (avgDolphins >= 2 * avgKoalas){
+    console.log(`Dolphines are the winners ${avgDolphins} vs ${avgKoalas}.`);
+  } else if (avgKoalas >= 2 * avgDolphins){
+    console.log(`Koalas are the winner ${avgDolphins} vs ${avgKoalas}.`);
+  } else {
+    console.log(`Nobody wins ${avgDolphins} vs ${avgKoalas}.`);
+  }
+}
+checkWinner(averageDolphines, averageKoalas);
+
+checkWinner(10, 1);
+
+//Data2:
+averageDolphines = (calcAverage(85, 54, 41)); //Dolphines
+averageKoalas = (calcAverage(23, 34, 27)); //Koalas
+console.log(averageDolphines, averageKoalas);
+checkWinner(averageDolphines, averageKoalas);
+
+*/
+
+
+/*--------------------------7 INTRODUCTION TO ARRAYS-------------------------*/
+/*
+const friend1 = `Michael`;
+const friend2 = `Steven`;
+const friend3 = `Peter`;
+
+const friends = [`Michael`, `Steven`, `Peter`];
+console.log(friends);
+
+// const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends [2] = `Jay`;
+console.log(friends);
+
+const firstName = `Jonas`;
+const jonas = [`firstName`, `Scmedtmann`, 2037 - 1991, `teacaher`, friends];
+console.log(jonas);
+console.log(jonas.length);
+
+
+//Exercise
+const calcAge = function(birthYear){
+  return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+*/
+
+
+
+/*--------------------------8 -------------------------*/
